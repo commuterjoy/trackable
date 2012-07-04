03985f9edb379e52fe19d4425e8c01cbd63bdcb6
Matt Chadburn <matt.chadburn@guardian.co.uk>
2012-07-04 09:18:05 +0100

    Added mustard test and handle non-existant elements

--- read.js
+++ read.js
@@ -1,6 +1,9 @@
 // Page load timestamp 
 var start = Date.now()
 
+// Support
+var mustard = (document.getBoundingClientRect && Array.map);
+
 // A thing on a page
 var Trackable = function(el) {
     this.trigged = false
@@ -9,6 +12,9 @@ var Trackable = function(el) {
 
 // Is the thing visible in the view port
 Trackable.prototype.inview = function() {
+    if (!this.el)
+        return;
+
     var rect = this.el.getBoundingClientRect()
     return (
         rect.top >= 0 &&
@@ -19,18 +25,18 @@ Trackable.prototype.inview = function() {
 }
 
 // Create a list of elements we want to fire triggers
-var tracks = [
+var things = [
     new Trackable(document.getElementById('story')),
-    new Trackable(document.getElementById('close'))
+    new Trackable(document.getElementById('close')),
     ]
 
 //
 window.onscroll = function() {
 
-    tracks.map(function(track) {
-        if (!track.triggered && track.inview()){
-            console.log(track.el.id + ',' + (Date.now() - start));
-            track.triggered = true; // ... and log to ophan
+    things.map(function(thing) {
+        if (!thing.triggered && thing.inview()){
+            console.log(thing.el.id + ',' + (Date.now() - start));
+            thing.triggered = true; // ... and log to ophan
         }
     });
 
